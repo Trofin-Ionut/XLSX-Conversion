111496c542357f27a3ff66ed193e86fdfae8afe4
Trofin George <George.Trofin@nbhx-trim.com>
2023-08-25 09:10:44 +0300

    xlsx conversion almost done

---
+++ XLSL Conversion/Models/Data.cs
@@ -0,0 +1,15 @@
+﻿namespace XLSL_Conversion.Models
+{
+    public class Data
+    {
+        public Dictionary<string, int> sum1 = new();
+        public Dictionary<string, int> sum2 = new();
+        public Dictionary<string, int> sum3 = new();
+        public void AddStorage(string storage)
+        {
+            sum1.TryAdd(storage, 0);
+            sum2.TryAdd(storage, 0);
+            sum3.TryAdd(storage, 0);
+        }
+    }
+}
